6e0fb93a52435ec6b47080dffcbad890ca8d077b
haah21 <hannahparis@naver.com>
2024-05-22 17:42:17 +0900

    .

--- 11.dump_practice.sh
+++ 11.dump_practice.sh
@@ -26,7 +26,28 @@ git push origin main
 # sudo apt-get install mariadb-server;
 # mariadb 서버 시작
 # sudo systemctl start mariadb;
-# mariadb 접속테스트
+
+# #local에서 sql 덤프 파일 생성
+# mysqldump -u root -p --default-character-set=utf8mb4 board > dumpfile.sql
+# #한글 깨질 때
+# mysqldump -uroot -p board -r dumpfile.sql
+
+# #dump파일을 github에 업로드
+
+# #리눅스에서 mariadb 설치
+# sudo apt-get install mariadb-server
+
+# #mariadb 서버 시작
+# sudo systemctl start mariadb
+
+# #mariadb 접속 테스트
 # sudo mariadb -u root -p
 
+# #git 설치
+# sudo apt install git
+
+# #git을 통해 repo clone
+# git clone 레파지토리
+
+# mysql -u root -p board < dumpfile.sql
 
